32298b37f2dd23660efe7bb02489560ecb3b540d
Austin Harrison <129778226+AustinH5544@users.noreply.github.com>
2025-06-01 19:25:07 -0700

    Changed starting script

--- start-all.ps1
+++ start-all.ps1
@@ -1,7 +1,9 @@
 Write-Host "Starting all services..."
 
-Start-Process powershell -ArgumentList "cd frontend; npm start"
-Start-Process powershell -ArgumentList "cd backend; dotnet run"
-Start-Process powershell -ArgumentList "cd ml-service; python -m uvicorn main:app --reload --port 5000"
+Start-Process powershell -ArgumentList "-NoExit", "cd frontend; npm start"
+Start-Process powershell -ArgumentList "-NoExit", "cd backend; dotnet run"
+Start-Process powershell -ArgumentList "-NoExit", "cd ml-service; .\.venv\Scripts\python.exe -m uvicorn main:app --reload --port 5000"
+
 
 Write-Host "All services started in new terminals."
+
